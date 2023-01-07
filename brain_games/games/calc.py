1ce594c1973a0034a858ec2633a0e66ea80ff7d3
artem <harchenkoart@gmail.com>
2023-01-07 12:59:41 +0300

    work on mistakes 4

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,14 +5,14 @@ game_rule = 'What is the result of the expression?'
 
 
 def current_game():
-    first_ramdom_number = random.randint(1, 100)
-    second_random_number = random.randint(1, 100)
+    fir_ramdom_number = random.randint(1, 100)
+    sec_random_number = random.randint(1, 100)
     random_symbol = random.choice([' + ', ' - ', ' * '])
-    question = str(first_ramdom_number) + random_symbol + str(second_random_number)
+    question = str(fir_ramdom_number) + random_symbol + str(sec_random_number)
     if random_symbol == ' + ':
-        correct_answer = str(first_ramdom_number + second_random_number)
+        correct_answer = str(fir_ramdom_number + sec_random_number)
     if random_symbol == ' - ':
-        correct_answer = str(first_ramdom_number - second_random_number)
+        correct_answer = str(fir_ramdom_number - sec_random_number)
     if random_symbol == ' * ':
-        correct_answer = str(first_ramdom_number * second_random_number)
+        correct_answer = str(fir_ramdom_number * sec_random_number)
     return question, correct_answer
--- brain_games/general.py
+++ brain_games/general.py
@@ -6,6 +6,7 @@ def body_games(game):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.game_rule)
+    wrong_answer = 'is wrong answer ;(. Correct answer was'
     i = 1
     while i <= 3:
         question, correct_answer = game.current_game()
@@ -15,7 +16,7 @@ def body_games(game):
             print('Correct!')
             i += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' {wrong_answer} '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             i += 10
         if i == 4:
