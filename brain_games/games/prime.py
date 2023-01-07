65e0a96a080b31c5ab5e94c8473ffc9d8566c681
artem <harchenkoart@gmail.com>
2023-01-07 11:17:02 +0300

    work on mistakes 3

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,14 +7,14 @@ game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def current_game():
     random_number = random.randint(1, 20)
     question = random_number
-    k = 0
-    for i in range(2, (random_number // 2) + 3):
-        if random_number % i == 0:
-            k += 1
-        if k == 0:
-            correct_answer = 'yes'
-        else:
-            correct_answer = 'no'
-    if random_number == 1:
+    correct_answer = 'yes'
+    if random_number <= 1:
         correct_answer = 'no'
+    else:
+        i = 2
+        while i <= sqrt(random_number):
+            if random_number % i == 0:
+                correct_answer = 'no'
+                break
+            i += 1
     return question, correct_answer
