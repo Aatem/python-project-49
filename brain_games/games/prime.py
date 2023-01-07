db5726e9962240485ff51ffaaed3d3f5f0ff0aef
artem <harchenkoart@gmail.com>
2023-01-07 10:28:03 +0300

    work on mistakes

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 import random
 
 
-game_rule = '"yes" if given number is prime. Otherwise anser "no".'
+game_rule = 'Answer "yes" if given number is prime. Otherwise anser "no".'
 
 
 def current_game():
